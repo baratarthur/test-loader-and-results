ac7628b912f0fe01995a0e5bd0efbd90cb0eb3c9
Arthur Barata <arthurpbarata@gmail.com>
2026-06-14 22:16:31 -0300

    change script to mark begging and test end

--- krabs/locust.sh
+++ krabs/locust.sh
@@ -1,1 +1,19 @@
-locust -f locust.py --headless -H http://192.168.5.20:30080 --csv="results_csv/$1"
+#!/bin/bash
+
+# Define a URL base da API
+API_URL="http://192.168.5.20:30080"
+FILENAME=$1
+
+# 1. Marca o início do teste nos logs da API
+echo "[SHELL] Sinalizando início do teste para a API..."
+echo curl -s -X GET "${API_URL}/"
+
+# 2. Executa o teste de carga com o Locust (bloqueante até o fim do Shape)
+echo "[SHELL] Iniciando o Locust em modo headless..."
+locust -f locust.py --headless -H "$API_URL" --csv="results_csv/${FILENAME}"
+
+# 3. Marca o término do teste nos logs da API quando o Locust finalizar
+echo "[SHELL] Locust finalizado. Sinalizando término do teste para a API..."
+echo curl -s -X GET "${API_URL}/end"
+
+echo "[SHELL] Teste concluído com sucesso!"
