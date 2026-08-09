e05d4b2281f8c5493f0382b8b164038a8033880f
Arthur Barata <a221738@dac.unicamp.br>
2026-08-09 14:56:39 -0300

    add python pod name

--- experiment1/cpu_memory_comsumption.sh
+++ experiment1/cpu_memory_comsumption.sh
@@ -1,4 +1,4 @@
 while true; do
-    echo "$(date +%s),$(kubectl top pod my-pod --no-headers | awk '{print $2","$3}')" >> metrics.csv
+    echo "$(date +%s),$(kubectl top pod social-network-python-6fdf865897-2tj2f --no-headers | awk '{print $2","$3}')" >> metrics.csv
     sleep 1
 done
