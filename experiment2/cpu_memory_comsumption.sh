32941db202c63e4cccdbdbafeddcd1787997c519
Arthur Barata <a221738@dac.unicamp.br>
2026-08-10 16:00:42 -0300

    setup experiment 2

--- experiment2/cpu_memory_comsumption.sh
+++ experiment2/cpu_memory_comsumption.sh
@@ -1,53 +1,7 @@
-#!/bin/bash
-
-OUTPUT="metrics.csv"
-
-echo "timestamp,dana_cpu,dana_mem,remote_cpu,remote_mem" > "$OUTPUT"
-
-MAIN_NAMESPACE="default"
-REMOTE_NAMESPACE="dana-remote-social-media-app-components"
-
+export POD_NAME=$(kubectl get pod -l app=dana-main -o jsonpath="{.items[0].metadata.name}")
+echo "timestamp,CPU,Memory" > metrics.csv
 for i in {1..60}; do
-
-    timestamp=$(date +%s)
-
-    # Main service
-    read dana_cpu dana_mem <<< $(
-        kubectl top pod -n $MAIN_NAMESPACE \
-        -l app=dana-main \
-        --no-headers | awk '{print $2,$3}'
-    )
-
-    # Sum all remote components
-    read remote_cpu remote_mem <<< $(
-        kubectl top pods -n $REMOTE_NAMESPACE --no-headers |
-        awk '
-        function cpu(v){
-            sub(/m/,"",v)
-            return v
-        }
-
-        function mem(v){
-            if(v~/Gi/){sub(/Gi/,"",v); return v*1024}
-            if(v~/Mi/){sub(/Mi/,"",v); return v}
-            if(v~/Ki/){sub(/Ki/,"",v); return v/1024}
-            return v
-        }
-
-        {
-            cpu_sum += cpu($2)
-            mem_sum += mem($3)
-        }
-
-        END{
-            print cpu_sum, mem_sum
-        }'
-    )
-
-    echo "${timestamp},${dana_cpu},${dana_mem},${remote_cpu}m,${remote_mem}Mi" >> "$OUTPUT"
-
-    echo "Collected sample $i"
-
+    echo "$(date +%s),$(kubectl top pod $POD_NAME --no-headers | awk '{print $2","$3}')" >> metrics.csv
+    echo "Measuring second $i"
     sleep 1
-
 done
