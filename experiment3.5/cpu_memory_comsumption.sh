bad54161ea731859c0e93fe1ff84f05fc1634da2
Arthur Barata <a221738@dac.unicamp.br>
2026-08-14 10:21:36 -0300

    add experiments 3.5 and 4.6

---
+++ experiment3.5/cpu_memory_comsumption.sh
@@ -0,0 +1,7 @@
+export POD_NAME=$(kubectl get pod -l app=dana-main -o jsonpath="{.items[0].metadata.name}")
+echo "timestamp,CPU,Memory" > metrics.csv
+for i in {1..160}; do
+    echo "$(date +%s),$(kubectl top pod $POD_NAME --no-headers | awk '{print $2","$3}')" >> metrics.csv
+    echo "Measuring second $i"
+    sleep 1
+done
---
+++ experiment4.6/cpu_memory_comsumption.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+OUTPUT="metrics.csv"
+
+echo "timestamp,dana_cpu,dana_mem,remote_cpu,remote_mem" > "$OUTPUT"
+
+MAIN_NAMESPACE="default"
+REMOTE_NAMESPACE="dana-remote-social-media-app-components"
+
+for i in {1..270}; do
+
+    timestamp=$(date +%s)
+
+    # Main service
+    read dana_cpu dana_mem <<< $(
+        kubectl top pod -n $MAIN_NAMESPACE \
+        -l app=dana-main \
+        --no-headers | awk '{print $2,$3}'
+    )
+
+    # Sum all remote components
+    read remote_cpu remote_mem <<< $(
+        kubectl top pods -n $REMOTE_NAMESPACE --no-headers |
+        awk '
+        function cpu(v){
+            sub(/m/,"",v)
+            return v
+        }
+
+        function mem(v){
+            if(v~/Gi/){sub(/Gi/,"",v); return v*1024}
+            if(v~/Mi/){sub(/Mi/,"",v); return v}
+            if(v~/Ki/){sub(/Ki/,"",v); return v/1024}
+            return v
+        }
+
+        {
+            cpu_sum += cpu($2)
+            mem_sum += mem($3)
+        }
+
+        END{
+            print cpu_sum, mem_sum
+        }'
+    )
+
+    echo "${timestamp},${dana_cpu},${dana_mem},${remote_cpu}m,${remote_mem}Mi" >> "$OUTPUT"
+
+    echo "Collected sample $i"
+
+    if [ $i -eq 150 ]; then
+        echo "Changing from app heavy to offload heavy..."
+    fi
+
+    sleep 1
+
+done
